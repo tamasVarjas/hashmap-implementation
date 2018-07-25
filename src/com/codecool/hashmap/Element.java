9079ff8c406cf30af2f7aedff653a1c101cc4991
Tamás Varjas <tamas.varjas00@gmail.com>
2018-07-25 10:28:15 +0200

    Element class

---
+++ src/com/codecool/hashmap/Element.java
@@ -0,0 +1,28 @@
+package com.codecool.hashmap;
+
+public class Element {
+
+    private String key;
+    private Integer value;
+
+    public Element(String key, Integer value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+}
